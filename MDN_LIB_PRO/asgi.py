b41dd6beb1c6344779a137c9f24cce10828832ca
ganpat singh <ganpatsingh@ganpats-MacBook-Air.local>
2021-04-17 11:43:00 +0530

    added to github

---
+++ MDN_LIB_PRO/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for MDN_LIB_PRO project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MDN_LIB_PRO.settings')
+
+application = get_asgi_application()
